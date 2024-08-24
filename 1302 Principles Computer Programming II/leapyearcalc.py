bbe87b3ea7624f3be0e0bca84b8542e12ce2aeae
JashThakkar <160945720+JashThakkar@users.noreply.github.com>
2024-08-23 22:32:22 -0400

    Create leapyearcalc.py

---
+++ 1302 Principles Computer Programming II/leapyearcalc.py
@@ -0,0 +1,26 @@
+year = input('Year: ')
+digits = False
+
+if int(year[2]) != 0:
+    digits = True
+  
+elif int(year[3]) != 0:
+    digits = True
+
+if digits:
+    remainder = int(year) % 4
+  
+    if remainder != 0:
+        print(year + " - not a leap year")
+      
+    elif remainder == 0:
+        print(year + ' - leap year')
+      
+else:
+    remainder = int(year) % 400
+  
+    if remainder == 0:
+        print(year + ' - leap year')
+      
+    elif remainder != 0:
+        print(year + " - not a leap year")
